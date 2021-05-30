fa2f19f25a3dbfc9a14fc05930e0d6b4d79ae6c0
fersoftware <fersoftware@gmail.com>
2021-05-30 19:05:53 -0300

    Bug - Adiciona validacao de obrigatoriedade e unicidade em Tag name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_30_213708) do
+ActiveRecord::Schema.define(version: 2021_05_30_215846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_05_30_213708) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
