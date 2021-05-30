c1b94ea0d3a71ae74db67864fdc7afcce02b92d2
fersoftware <fersoftware@gmail.com>
2021-05-30 18:56:28 -0300

    Adiciona modelo Tag
    Extra: Adiciona has-many :contents no modelo User

---
+++ db/migrate/20210530213708_create_tags.rb
@@ -0,0 +1,12 @@
+class CreateTags < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tags do |t|
+      enable_extension('citext')
+      t.citext :name
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+    add_index :tags, [:name, :user_id], unique:true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_30_211929) do
+ActiveRecord::Schema.define(version: 2021_05_30_213708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2021_05_30_211929) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_contents_on_user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tags_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_05_30_211929) do
   end
 
   add_foreign_key "contents", "users"
+  add_foreign_key "tags", "users"
 end
