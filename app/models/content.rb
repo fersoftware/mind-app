c4f152bc178f856d49c4b7421c6a08b0d2511efa
fersoftware <fersoftware@gmail.com>
2021-05-30 19:28:45 -0300

    Bug - Adiciona depedent destroy em Tag e Content

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,6 +2,6 @@ class Content < ApplicationRecord
   belongs_to :user
 
   validates :title, :description, presence: true
-  has_many :tag_contents
+  has_many :tag_contents, dependent: :destroy
   has_many :tags, through: :tag_contents
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
   belongs_to :user
   validates :name, presence: true, uniqueness: {scope: :user_id}
-  has_many :tag_contents
+  has_many :tag_contents, dependent: :destroy
   has_many :contents, through: :tag_contents
 end
