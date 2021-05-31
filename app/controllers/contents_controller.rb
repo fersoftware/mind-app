b8669fb9977b4c82b8e9e7e076621c0c2ed92ef3
fersoftware <fersoftware@gmail.com>
2021-05-30 22:01:16 -0300

    Adicionado form de cadastro de conteudo

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,8 +1,14 @@
 class ContentsController < ApplicationController
     before_action :authenticate_user!
     include Devise::Controllers::Helpers 
+
     def index
         @contents = current_user.contents
     end
+
+    def new
+        @content = Content.new
+    end
+    
 end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
-  get 'home/index'
+  get 'home', to: 'home#index'
   resources :contents  
 end
